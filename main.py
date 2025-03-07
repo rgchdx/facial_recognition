f0681c18c9ff3dc54fb43b17a8c0395965cc950a
Richie Dix <rgchdx@gmail.com>
2025-03-07 09:21:05 -0600

    Imported the mode images into a list with thieir paths

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import cv2
 
 ### Webcam setup ###
@@ -6,6 +7,14 @@ cap.set(3, 680)  # Set width slightly smaller than 720
 cap.set(4, 510)  # Set height slightly smaller than 540
 
 imgBackground = cv2.imread("Resources/background.png")
+
+###Importing the mode images into a list###
+folderModePath = 'Resources/Modes'
+modePathList = os.listdir(folderModePath) #obtain all of the files in the folder and put it in a list
+imgModeList = [] #create empty list to store all the images
+for path in modePathList: #iterate through the list of files
+    imgModeList.append(cv2.imread(os.path.join(folderModePath,path))) #we use cv2 to read the images and appepend it to the list. Use os.path.join to get the full path of the image
+#print(len(imgModeList))
 
 ### What the webcam will show ###
 while True:
