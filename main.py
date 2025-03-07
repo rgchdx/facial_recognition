ceef75d014a5d0e4f6d2cec7ca3663d355fa3418
Richie Dix <rgchdx@gmail.com>
2025-03-07 08:47:58 -0600

    addded images in resources

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ cap = cv2.VideoCapture(0) #if using multiple cameras, have 1 instead of 0
 cap.set(3,1280,) #set width of the frame
 cap.set(4,720) #set height of the frame
 
+###What the webcam will show###
 while True:
     successs, img = cap.read()
     cv2.imshow("Face Attendance",img)
