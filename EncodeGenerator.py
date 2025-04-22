723a052537c05ee2eb22796a7598c7fcfcb0da9e
Richie Dix <rgchdx@gmail.com>
2025-04-22 00:43:23 -0500

    Created the encodeList finder so that it will take the image and get the encoded images for it and return the list

--- EncodeGenerator.py
+++ EncodeGenerator.py
@@ -13,4 +13,17 @@ for path in pathList:  # iterate through the list of files
     imgList.append(cv2.imread(os.path.join(folderPath, path)))
     faceIds.append(os.path.splitext(path)[0])  # we use cv2 to read the images and append it to the list. Use os.path.join to get the full path of the image
     #Splitting to get the id and png part separated. The 0 will only extract the id since it will be the first part
-print(len(imgList))
+print(faceIds)
+
+def findEncoding(imgList):
+    encodeList = []  # create empty list to store all the encodings
+    for image in imgList:
+        img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # convert the image from BGR to RGB since cv uses BGR by default
+        encode = face_recognition.face_encodings(img)[0] # find the face encodings of the image
+        encodeList.append(encode)
+    return encodeList
+
+print('Encoding Started')
+encodeListKnown = findEncoding(imgList)
+print('Encoding Complete')
+        
